bcdf4b889bcc3699e8fcf08b9dd603bb0f96bc0d
benemues <143995569+benemues@users.noreply.github.com>
2025-04-03 07:47:17 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let rng: usize = rand::rng().random_range(1..10);
+
+    let stdin = std::io::stdin();
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf);
+
+    if buf.trim().parse::<usize>().unwrap() == rng {
+        println!("Guessed correctly")
+    } else {
+        println!("Incorrect, the number was {rng}")
+    }
+
+
+
 }
+
