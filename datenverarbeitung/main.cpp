940ba51d5964011c0feab75acf301620edc4f99a
benemues <143995569+benemues@users.noreply.github.com>
2025-09-22 19:08:20 +0200

    Hallo welt

--- datenverarbeitung/main.cpp
+++ datenverarbeitung/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>;
 using namespace std;
 
+class I2B{
+    public:
+        int hallo;
+        static int hallowelt() {
+            cout << "Hallo Welt" << endl;
+        }
+};
+
 int main() {
-    cout << "hallo welt" << endl;
+    I2B i2b;
+    i2b.hallo = 5;
+    I2B::hallowelt();
 }
